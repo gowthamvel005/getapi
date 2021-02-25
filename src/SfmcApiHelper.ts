3b8361913596242f2ea83c9d32233aefb7813f01
gowthamvel005 <gowtham.pashtek@gmail.com>
2021-02-25 11:54:42 +0530

    changes

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -76,8 +76,10 @@ export default class SfmcApiHelper
             axios.post(sfmcAuthServiceApiUrl, postBody, {"headers" : headers})
             .then((response: any) => {
                 Utils.logInfo("header11 token==>"+response);
+                var myobj1= JSON.stringify(response);
+                Utils.logInfo("header12 token==>"+myobj1);
                 // success
-                Utils.logInfo("hello token==>"+ response.data.accessToken);
+                Utils.logInfo("hello token==>"+ myobj1.data.accessToken);
                 
                 let accessToken = response.data.accessToken;
                 Utils.logInfo("ENND token==>"+ response.data.accessToken);
